65f99115c8cd20bc2eb1519afc1b33b00f0ab1d5
humdeepsingh <55726348+humdeepsingh@users.noreply.github.com>
2019-11-10 13:01:10 +1030

    Update registeredentity.h
    added data members and member functions

--- Assignmnet2/Application/registeredentity.h
+++ Assignmnet2/Application/registeredentity.h
@@ -6,12 +6,22 @@
 class RegisteredEntity : public QObject
 {
     Q_OBJECT
+private:
+    QString _id;
+    QDateTime _lastEditTime;
+    QString _lastEditedBy;
+
 public:
     explicit RegisteredEntity(QObject *parent = nullptr);
+    QString getId() const;
+    QDateTime getLastEditTime() const;
+    QString getLastEditedBy() const;
 
-signals:
-
-public slots:
+protected:
+    RegisteredEntity( QObject *parent, QString id);
+    void setId(const QString &value);
+    void setLastEditTime(const QDateTime &value);
+    void setLastEditedBy(const QString &value);
 };
 
 #endif // REGISTEREDENTITY_H
