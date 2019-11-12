9deced3c793937ea3ce8e02dcc5f4705605895d6
humdeepsingh <55726348+humdeepsingh@users.noreply.github.com>
2019-11-12 13:19:18 +1030

    Update assettype.h

--- Assignmnet2/Application/assettype.h
+++ Assignmnet2/Application/assettype.h
@@ -1,12 +1,28 @@
 #ifndef ASSETTYPE_H
 #define ASSETTYPE_H
+
 #include "registeredentity.h"
+#include "asset.h"
+#include "userpropertydefinition.h"
 
 
 class AssetType : public RegisteredEntity
 {
 public:
-    AssetType();
+    AssetType(QString id, QObject *parent);
+
+    QString name() const;
+    void setName(const QString &name);
+
+private:
+    QString _name;
+    QVector <Asset> instances;
+
+private slots:
+//error
+    void instanceDestroyed(Asset instance);
+
 };
+
 
 #endif // ASSETTYPE_H
