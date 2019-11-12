c6709f14264c85eddf9d99e63458c8cb11b83541
humdeepsingh <55726348+humdeepsingh@users.noreply.github.com>
2019-11-12 13:33:28 +1030

    Update asset.cpp

--- Assignmnet2/Application/asset.cpp
+++ Assignmnet2/Application/asset.cpp
@@ -1,5 +1,56 @@
 #include "asset.h"
-Asset::Asset()
+
+Asset::Asset(QString id, QObject *parent): RegisteredEntity (id, parent)
 {
 
+}
+
+QString Asset::serialNo() const
+{
+    return _serialNo;
+}
+
+void Asset::setSerialNo(const QString &serialNo)
+{
+    _serialNo = serialNo;
+}
+
+QString Asset::brand() const
+{
+    return _brand;
+}
+
+void Asset::setBrand(const QString &brand)
+{
+    _brand = brand;
+}
+
+QString Asset::model() const
+{
+    return _model;
+}
+
+void Asset::setModel(const QString &model)
+{
+    _model = model;
+}
+
+QDateTime Asset::purchaseDate() const
+{
+    return _purchaseDate;
+}
+
+void Asset::setPurchaseDate(const QDateTime &purchaseDate)
+{
+    _purchaseDate = purchaseDate;
+}
+
+QDateTime Asset::DisposalDate() const
+{
+    return _DisposalDate;
+}
+
+void Asset::setDisposalDate(const QDateTime &DisposalDate)
+{
+    _DisposalDate = DisposalDate;
 }
