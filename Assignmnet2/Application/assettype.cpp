32812c17660bd99bb865306f43165d0dd901e288
humdeepsingh <55726348+humdeepsingh@users.noreply.github.com>
2019-11-12 13:20:04 +1030

    Update assettype.cpp

--- Assignmnet2/Application/assettype.cpp
+++ Assignmnet2/Application/assettype.cpp
@@ -1,6 +1,20 @@
 #include "assettype.h"
 
-AssetType::AssetType()
+AssetType::AssetType(QString id, QObject *parent): RegisteredEntity (id, parent)
 {
 
+}
+
+/**
+ * @brief AssetType::name
+ * @return the name of the asset
+ */
+QString AssetType::name() const
+{
+    return _name;
+}
+
+void AssetType::setName(const QString &name)
+{
+    _name = name;
 }
